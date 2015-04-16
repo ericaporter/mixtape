5454503a15db987c9d19b61413e4ebd8d4b71221
ericaporter <ericaporter001@gmail.com>
2015-04-16 15:45:46 +0100

    js play button added

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Projectonedraft
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.assets.precompile += %w(soundmanager2.swf soundmanager2_flash9.swf)
   end
 end
